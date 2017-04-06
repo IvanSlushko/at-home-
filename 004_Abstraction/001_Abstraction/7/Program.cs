3a04ecc4890b22581a28b32e7701171d8eeb2d72
IvanSlushko <lu813@tut.by>
2017-04-06 23:55:23 +0300

    no message

---
+++ 004_Abstraction/001_Abstraction/7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    interface IGoEater
+    {
+        void Go();
+        void Eat();
+    }
+    class Dog:IGoEater 
+    {
+        public void Go()
+        {
+            Console.WriteLine("dog step");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("dog eat");
+        }
+        public void Guard()
+        {
+            Console.WriteLine("dog guard");
+        }
+    }
+
+    class Cat:IGoEater
+    {
+        public void Go()
+        {
+            Console.WriteLine("cat step");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("cat eat");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            dog.Go(); //однотипный объекты
+            dog.Eat();
+            //dog.Guard();
+
+            cat.Go(); //однотипный объекты
+            cat.Eat();
+
+            Console.WriteLine("------------");
+            IGoEater dog1 = new Dog();
+            dog1.Eat();
+            dog1.Go();
+
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Dog
+    {
+        public void Go()
+        {
+            Console.WriteLine("dog step");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("dog eat");
+        }
+        public void Guard()
+        {
+            Console.WriteLine("dog guard");
+        }
+    }
+
+    class Cat
+    {
+        public void Go()
+        {
+            Console.WriteLine("cat step");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("cat eat");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            dog.Go(); //однотипный объекты
+            dog.Eat();
+            dog.Guard();
+
+            cat.Go(); //однотипный объекты
+            cat.Eat();
+
+
+        }
+    }
+}
