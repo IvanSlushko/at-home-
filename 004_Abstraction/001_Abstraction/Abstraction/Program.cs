8ae0a2a373ba8733096c92459745e21fc7b39ec4
IvanSlushko <lu813@tut.by>
2017-04-06 23:40:20 +0300

    no message

---
+++ 004_Abstraction/001_Abstraction/Abstraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Абстрактный класс.
+
+namespace Abstraction
+{
+    // Абстрактный класс.
+    abstract class AbstractClass
+    {
+        public abstract void Method();
+    }
+
+    // Конкретный класс.
+    class ConcreteClass : AbstractClass //наследуемся от абстрактного
+    {
+        public override void Method()  // переопределяем метод абстрактного власса
+        {
+            Console.WriteLine("Implementation");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClass instance = new ConcreteClass();
+
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
